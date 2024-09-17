1d1ffc156c6ec69749e6454beec73f15d69437db
ShadowMega1337 <alessandro.hauser@bbzbl-it.ch>
2024-09-17 10:23:37 +0200

    init

---
+++ src/main/java/ch/junggarde/api/PingResource.java
@@ -0,0 +1,15 @@
+package ch.junggarde.api;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+@Path("/ping")
+@ApplicationScoped
+public class PingResource {
+    @GET
+    public Response ping() {
+        return Response.ok("pong").build();
+    }
+}
